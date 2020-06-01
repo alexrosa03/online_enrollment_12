ffe3207a328d772699b8db7afdbd2ad63734fa65
Alex <delarosauberstrike@gmail.com>
2020-06-01 14:12:57 +0800

    changes are made

--- public/js/app.js
+++ public/js/app.js
@@ -49839,8 +49839,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! D:\xampp\htdocs\enrollment\enrollemnt_project\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! D:\xampp\htdocs\enrollment\enrollemnt_project\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\xampp\htdocs\online_enrollment_12\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\xampp\htdocs\online_enrollment_12\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
